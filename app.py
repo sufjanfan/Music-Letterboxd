85981da7d7c6249201f7cb50ebcc9e72212833cc
sufjanfan <hding@college.harvard.edu>
2024-12-06 22:30:48 +0000

    Fri, Dec 6, 2024, 5:30 PM -05:00

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///database.db")
+db = SQL("sqlite:///songs.db")
 
 @app.route("/")
 def index():
