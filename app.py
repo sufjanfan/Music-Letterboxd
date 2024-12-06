925697e9fccd8c86677983afbf7586f57a81cfb8
sufjanfan <hding@college.harvard.edu>
2024-12-06 23:10:50 +0000

    Fri, Dec 6, 2024, 6:10 PM -05:00

--- app.py
+++ app.py
@@ -23,6 +23,45 @@ db = SQL("sqlite:///songs.db")
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+'''
 # Login
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -41,7 +80,39 @@ def login():
         return redirect("/profile")
 
     return render_template("login.html")
+'''
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # get form data using post method
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        # makes sure fields/data category exist
+        if not username:
+            return apology("must provide username")
+        if not password:
+            return apology("must provide password")
+        if not confirmation:
+            return apology("must provide confirmation")
+        # makes sure password and confirmation match
+        if password != confirmation:
+            return apology("passwords do not match")
+        # hashes password so that actual password is not submitted
+        # inserts user into database
+        try:
+            hash_password = generate_password_hash(password)
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash_password)
+        except ValueError:
+            return apology("username already exists")
+        return redirect("/login")
+    # go to registration page/form for GET requests
+    else:
+        return render_template("register.html")
+    
+'''
 # Register
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -64,6 +135,7 @@ def register():
         return redirect("/login")
 
     return render_template("register.html")
+    '''
 
 # Logout
 @app.route("/logout")
