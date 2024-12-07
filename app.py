aecaaac13950b64e543b3f18e6cd836dbeceecc8
kfang625 <75137240+kfang625@users.noreply.github.com>
2024-12-07 03:19:43 +0000

    Fri, Dec 6, 2024, 10:19 PM -05:00

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def profile():
 
     # For GET requests, show user reviews
     reviews = db.execute("SELECT * FROM reviews WHERE user_id = ?", session["user_id"])
-    return render_template("profile.html", name=username, reviews=reviews)
+    return render_template("profile.html", reviews=reviews)
 
 @app.route("/review", methods=["POST"])
 @login_required
